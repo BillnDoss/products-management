e3812d285f1fd22bac63478f9a39b47f09cf9fc5
Jacky Tan <jackytan06028@gmail.com>
2026-09-21 14:44:09 +0800

    created frontend page for login and registration

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoute");
 const productRoutes = require("./routes/productRoute");
+const cors = require("cors");
 
 mongoose
     .connect("mongodb://localhost:27017/jwt_with_products")
@@ -12,6 +13,16 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
+
+const corsHandler = cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200,
+    preflightContinue: true,
+});
+
+app.use(corsHandler);
 
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
---
+++ frontend/products-management/src/pages/Registration.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import "./Login.css";
+import axios from "axios";
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // 💡 Add your login / authentication logic here
+        console.log("Form submitted:", { email, password });
+        try {
+            const response = await axios.post("http://localhost:5000/users/register", {
+                email,
+                password,
+            });
+            console.log("Registration successful: ", response.data);
+            alert("Registration Successful!");
+        } catch (error) {
+            console.log("Registration Error: ", error);
+        }
+    };
+
+    return (
+        <div className="login-wrapper">
+            <form onSubmit={handleSubmit} className="login-card">
+                <h2>Please Register here</h2>
+
+                <div className="form-group">
+                    <label htmlFor="email">Email Address</label>
+                    <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" required />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••••" required />
+                </div>
+
+                <button type="submit" className="login-btn">
+                    Register now
+                </button>
+
+                <button className="login-btn" onClick={() => navigate("/")}>
+                    Back to Login
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
